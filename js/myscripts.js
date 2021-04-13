c9a877bf7b32300bd13af9c2d08e7e77c207c741
Christopher Regan <ChristopherReganJD@Gmail.com>
2021-04-13 11:31:54 -0400

    combined css into one changed all references, added lang

--- js/myscripts.js
+++ js/myscripts.js
@@ -3,9 +3,11 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 241 || document.documentElement.scrollTop > 241) {
     document.getElementById("littlelogo").style.display = "inline";
-	
-  
+    document.getElementById("full-navbar-index").style.justifyContent  = "space-between";
+
+
   } else {
     document.getElementById("littlelogo").style.display = "none";
+    document.getElementById("full-navbar-index").style.justifyContent  = "center";
   }
 }
