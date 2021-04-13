ad03d7b99c582b15889d669a515f33921d812caa
Christopher Regan <ChristopherReganJD@Gmail.com>
2021-04-13 12:27:14 -0400

    fixed sticky error that came from combining css and fixed font reference

--- js/myscripts.js
+++ js/myscripts.js
@@ -2,12 +2,13 @@
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 241 || document.documentElement.scrollTop > 241) {
-    document.getElementById("littlelogo").style.display = "inline";
+    document.getElementById("smallimageindexpage").style.display = "inline";
     document.getElementById("full-navbar-index").style.justifyContent  = "space-between";
-
+    document.getElementById("navbar").style.position  = "static";
 
   } else {
-    document.getElementById("littlelogo").style.display = "none";
+    document.getElementById("smallimageindexpage").style.display = "none";
     document.getElementById("full-navbar-index").style.justifyContent  = "center";
+    document.getElementById("navbar").style.position  = "static";
   }
 }
